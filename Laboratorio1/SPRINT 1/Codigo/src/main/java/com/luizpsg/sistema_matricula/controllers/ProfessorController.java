95f62cb33d6cab1609929d14b251722cf65bc244
luizpsg <luiz.saud@sga.pucminas.br>
2025-03-07 20:40:29 -0300

    refactor: remove unused removerDisciplina endpoint in ProfessorController
    This commit removes the deprecated `/remover-disciplina` endpoint from the ProfessorController, which was likely no longer needed after implementing more robust discipline management methods.

--- Laboratorio1/SPRINT 1/Codigo/src/main/java/com/luizpsg/sistema_matricula/controllers/ProfessorController.java
+++ Laboratorio1/SPRINT 1/Codigo/src/main/java/com/luizpsg/sistema_matricula/controllers/ProfessorController.java
@@ -72,16 +72,6 @@ public class ProfessorController {
     return professorRepository.saveAll(professores);
   }
 
-  @PostMapping("/remover-disciplina")
-  public Professor removerDisciplina(@RequestParam Long professorId, @RequestParam Long disciplinaId) {
-    Professor professor = professorRepository.findById(professorId).get();
-    Disciplina disciplina = disciplinaRepository.findById(disciplinaId).get();
-    professor.removeDisciplina(disciplina);
-    disciplina.setProfessor(null);
-    disciplinaRepository.save(disciplina);
-    return professorRepository.save(professor);
-  }
-
   @PostMapping("/adicionar-disciplina")
   public Professor adicionarDisciplina(@RequestParam Long professorId, @RequestParam Long disciplinaId) {
     LOGGER.info("Adicionando disciplina ID: " + disciplinaId + " ao professor ID: " + professorId);
